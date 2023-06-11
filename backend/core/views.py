c76c7292cf93ad0db538882462ae59e140fd0713
Luan Alves <97049835+Luan029@users.noreply.github.com>
2023-06-11 11:17:09 -0300

    Foi criado o endpoint de Listagem e o endpoint de items

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import ListSerializer, ItemSerializer
+from .models import List, Item
+
+
+class ListViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ItemViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
